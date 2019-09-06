be022945fd8374dea255faea0afccc04c852fd4f
Vitalii.Dziubenko <vitalii.dziubenko@onseo.biz>
2019-09-06 23:26:05 +0300

    Refactoring #4.4

--- src/test/java/com/qa/google/base/BaseTest.java
+++ src/test/java/com/qa/google/base/BaseTest.java
@@ -1,23 +1,18 @@
 package com.qa.google.base;
 
 import io.qameta.allure.Attachment;
-import org.openqa.selenium.Dimension;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeMethod;
-
-import java.util.concurrent.TimeUnit;
 
 import static com.qa.google.base.Reporter.log;
 import static com.qa.google.base.TestManager.baseUrl;
 import static com.qa.google.init.DriverManager.getDriver;
 
-public class BaseTest extends BasePage {
+public class BaseTest {
 
     public void navigateToBaseUrl() {
         log("Navigating to BaseUrl");
